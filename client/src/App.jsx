c0b9e558d09862f19f310b559688197bd04dbb4a
Ashish Kumar Patel <ashishkumarp@zignuts.com>
2024-05-16 12:25:31 +0530

    Client Routes Setup and server

---
+++ client/src/App.jsx
@@ -0,0 +1,26 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home" 
+import About from "./pages/About" 
+import SignUp from "./pages/SignUp" 
+import SignIn from "./pages/SignIn" 
+import Dashboard from "./pages/Dashboard" 
+import Projects from "./pages/Projects" 
+
+
+function App() {
+	return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
